c14ffbcec175cab7681c7d45f2e0c124d22093b3
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-23 16:00:02 -0600

    Incluida la solución

--- David Martínez/Easy/Array/Contains_Duplicate.cpp
+++ David Martínez/Easy/Array/Contains_Duplicate.cpp
@@ -1,3 +1,63 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        //1)Listen
+            //Me llega un array de integers llamado nums
+            //Regreso true si existen elementos duplicados
+            //Regreso false si todos los elementos son distintos
+            //1 <= nums.length <= 105
+            //-10^9 <= nums[i] <= 10^9 (1,000,000,000)
+        
+        //2)Example
+            //Cuaderno
+        
+        //3)Brute Force
+            //Mi primer idea es la de usar hashtables
+            //Para recorrer la lista una vez y saber la frecuencia(veces que aparece) de los numeros
+            //Tendría time:O(N)
+            //Tendría Memory: O(N)
+            //Siento que esto es mejor a usar un fast y un slow pointer, sería time:O(N^2), memory:O(1)
+        
+        //4)Optimize
+            //Usar Hashtables para tener conocimiento de los numeros y la frecuencia
+            //Como puedo usar hashtables en c++? Puede ser con un set o map
+        
+        //5)Walkthrough
+            //Recorro la lista nums y voy metiendo los datos al set
+            //Si el dato ya existe dentro de set, regreso true
+            //Si recorro nums hasta el final, regreso false
+        
+        //6)Implement
+        
+        //7)Test
+            //Set de 1 valor
+            //Set con valores no repetidos
+            //Set con valores repetidos
+        
+        set<int, greater<int> > values;
+        values.insert(1000000001);//En las condiciones, ningun valor tendra ese valor
+                                //Mi set es de menor a mayor
+        
+        //El find de set, cuando no encuentra el elemento regresa el ultimo
+        //Entonces haré que el ultimo sea uno que no podamos acceder
+        
+        for(int i = 0; i < nums.size(); i++){//Checa cada valor de nums
+            
+            if(values.find(nums[i]) == values.end()){//Si me manda a la ubicación de 1000000001
+                                                    //Significa que no existe el dato 
+                values.insert(nums[i]);  //Agregamos el dato al set
+            }
+            else{   //Si existe el dato
+                return true;    //Regreso false
+            }
+        }
+        return false;   //Si llego al final, significa que no se repiten caracteres y regreso false
+        
+        
+    }
+};
+

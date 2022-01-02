a1c6675b56ec3b324e1cb8f61f4c455dc07728af
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-02 16:24:55 -0600

    Agregado link de la solución

--- David Martínez/Easy/Array/Single_Number.cpp
+++ David Martínez/Easy/Array/Single_Number.cpp
@@ -20,9 +20,11 @@ public:
 Nota:
     Esta solución la saque de youtube, no tenía idea que se podia representar el xor de tal manera.
     Aparte lo resuelve dentro de un solo espacio de memoria.
+    Solución explicada: https://www.youtube.com/watch?v=krgK0UlfNYY&t=249s
 
     Esto representa la ayuda que es el saber manipular bits.
     Debo recordar, checar este tema con los demás.
 
     Debo tratar de solucionar la respuesta por mi cuenta
+
 */

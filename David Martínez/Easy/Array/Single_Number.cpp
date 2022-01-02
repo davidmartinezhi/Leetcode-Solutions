405853e15be570e39cfa1259fe679f1c8e40f6b3
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-02 16:23:50 -0600

    Agregada solucion single number

---
+++ David Martínez/Easy/Array/Single_Number.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int i = 0; i < nums.size(); i++){
+            result ^= nums[i];
+        }
+        
+        return result;
+    }
+};
+
+/*
+Nota:
+    Esta solución la saque de youtube, no tenía idea que se podia representar el xor de tal manera.
+    Aparte lo resuelve dentro de un solo espacio de memoria.
+
+    Esto representa la ayuda que es el saber manipular bits.
+    Debo recordar, checar este tema con los demás.
+
+    Debo tratar de solucionar la respuesta por mi cuenta
+*/

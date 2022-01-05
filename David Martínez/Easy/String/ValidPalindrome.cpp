ea756fc24f0638622e19ee4989f3b33c9d3d634f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-05 17:39:50 -0600

    Agregadas notas de la solución

--- David Martínez/Easy/String/ValidPalindrome.cpp
+++ David Martínez/Easy/String/ValidPalindrome.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 
+
+/*
+Nota:
+    -Solución sacada en 31 minutos
+    -Solución bastante eficiente
+    -Deberia de memori<ar como transformar a minusculas o mayusculas. Algún string.
+    - También es bueno saber de la función isalnum y correspondientes. Han de ser muy utiles.
+
+*/

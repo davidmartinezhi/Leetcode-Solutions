be7df95fca875fb9c7fd3036349618262f59ef47
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-11 19:49:55 -0600

    Agregada solucion con strings auxiliares

--- David Martínez/Easy/String/LongestCommonPrefix.cpp
+++ David Martínez/Easy/String/LongestCommonPrefix.cpp
@@ -22,8 +22,8 @@ public:
             //La siguiente palabra checar las letras que son iguales, cuando exista una dif la borro
             //Hago lo mismo para las demas, pero solo checo el size del array de prefix
             //Al final regreso todos los elementos en el array, juntos
-            //memory: O(n)
-            //runtime: O(n * prefix)
+            //memory: O(m)
+            //runtime: O(n * m)
         
         // optimize
             //Si el array tiene un elemento, retorno ese elemento
@@ -35,15 +35,13 @@ public:
             //Asi siempre tengo el dato de hasta donde recorrer, pero mantengo memory O(1)
             //Runtime BCR: O(elementos de la lista * prefix) O(n log n)
         
-        // walkthrough
-            //Checo si el array tiene 1 elemento, si si, regreso el elemento dentro
-            //Creo entero para el indice
-            //Comparo las primeras dos palabras hasta que no sean iguales
-            //Ahora creo un for loop de todas las palabras restantes
-                //Si la palabra esta vacía, regreso ""
-                //Si la palabra es diferente de su antecesor en un char o llego al final, recorro el apuntador a izq
+            //Simplify and generalize
+                //Primero lo aplicare con 2 palabras nadamas, el recorrido
+                
+            
         
-            //Si el apuntador llega a -1, regreso ""
+        // walkthrough
+            // 
         
         // implement
             //
@@ -59,48 +57,33 @@ public:
         //Si es de un elemento, regreso ese elemento
         if(strs.size() == 1){ return strs[0]; }
         
-        //Creo apuntador al valor donde termina el prefix.
-        //Su primer valor es el size del primer elemento de la lista
-        int prefixPosition = strs[0].size();
+        string result = strs[0];
         
-        //Si es 0, regreso un empty string
-        if(prefixPosition == 0){ return ""; }
-        
-        //Recorro la lista
-        for(int i = 1; i < strs.size(); i++){
+        for(int i = 0; i < strs.size()-1; i++){
             
-            //Checo si la palabra está vacía
-            if(strs[i].size() == 0){ return ""; }
+            string aux;
             
-            //Recorro la palabra
-            int size = (strs[i].size() > strs[i-1].size()) ? strs[i-1].size() : strs[i].size();
+            //Saco la palabra de menor tamaño, para no pasarme de indice
+            int size = (strs[i].size() > strs[i+1].size()) ? strs[i+1].size() : strs[i].size();
+            
             for(int j = 0; j < size; j++){
-
-                //Checo hasta donde coincide con la palabra anterior
-                if(strs[i][j] != strs[i-1][j]){
-                    
-                    if(prefixPosition > j -1){  //Si el prefix es mayor, le damos el valor
-                        prefixPosition = j-1;   
-                    }
-                    
-                    break;//Dejamos de recorrer las letras de la palabra
+                if(strs[i+1][j] == strs[i][j]){
+                    aux += strs[i][j];
                 }
-                
-                //Si el prefix es menor a 0, regreso string vacío
-                if(prefixPosition == -1){ return ""; }
+                else{
+                    break;
+                }
             }
+            cout << "aux: " <<  aux << endl;
+            if(aux == ""){return "";}
+            
+            result = (aux.size() > result.size()) ? result: aux;
+            
         }
-        
-        string result;
-        for(int i = 0; i <= prefixPosition; i++){
-            result += strs[0][i];
-        }
-        cout << prefixPosition << endl;
-       
+
         return result;
     }
 };
-
 /*
     Es muy sencillo, pero no estaba pensando claro.
 

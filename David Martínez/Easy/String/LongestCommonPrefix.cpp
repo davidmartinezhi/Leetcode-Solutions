b72169673d05d4fd3ae83445f2178880559c4307
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-12 12:50:58 -0600

    Agregada solucion optimizada

--- David Martínez/Easy/String/LongestCommonPrefix.cpp
+++ David Martínez/Easy/String/LongestCommonPrefix.cpp
@@ -37,7 +37,10 @@ public:
         
             //Simplify and generalize
                 //Primero lo aplicare con 2 palabras nadamas, el recorrido
-                
+                //Luego lo haré con un recorrido de sliding window technique
+            
+            //Puedo usar un string auxiliar y ese compararlo con el result
+            //
             
         
         // walkthrough
@@ -52,37 +55,61 @@ public:
             //Special case:
                 //Array con 1 elemento
                 //Palabras que no coinciden
+                //Strs con strings vacíos
+        
+        
+        
+        //Runtime: O(n * m) n: elementos en la lista, m: tamaño de prefix
+        //Memory: O(1)
+
+
         
         //Checo el size del array
         //Si es de un elemento, regreso ese elemento
         if(strs.size() == 1){ return strs[0]; }
         
+        //Si tiene más de un elemento 
+        //El primer prefix es la primer palabra completa
         string result = strs[0];
         
+        //Recorremos la lista de strings
         for(int i = 0; i < strs.size()-1; i++){
             
-            string aux;
+            string aux; //String auxiliar
             
             //Saco la palabra de menor tamaño, para no pasarme de indice
             int size = (strs[i].size() > strs[i+1].size()) ? strs[i+1].size() : strs[i].size();
+            //Saco si el prefix tiene menor tamaño que la palabra
+            size = (size > result.size()) ? result.size() : size;
             
+            //De esta manera el size siempre nos va a hacer recorrer los menor carácteres posibles
+            
+            //Recorro el size
             for(int j = 0; j < size; j++){
+                
+                //Si las letras coinciden
                 if(strs[i+1][j] == strs[i][j]){
-                    aux += strs[i][j];
+                    aux += strs[i][j];  //La agregamos al string auxiliar
                 }
                 else{
-                    break;
+                    break;  //Cuando no coinciden, dejamos de checar
                 }
             }
-            cout << "aux: " <<  aux << endl;
-            if(aux == ""){return "";}
             
-            result = (aux.size() > result.size()) ? result: aux;
+            //Si la primer letra no coincidío y el aux esta vacío
+            if(aux == ""){return "";}   //Regresamos un string vacío
             
+            //El resultado es el string con menor tamaño entre aux y el resultado
+            result = (aux.size() > result.size()) ? result: aux; 
         }
 
-        return result;
-    }
+        return result;  //Regresamos el string resultante
+        
+        
+        //Idea de Optimización, el mío ya hace lo mismo pero yo lo hago al trabajar en size, no string
+        //Puedo hacer la primer palabra, el prefix
+        //Y despues solo ir restando letras
+    }    
 };
 /*
     Es muy sencillo, pero no estaba pensando claro.

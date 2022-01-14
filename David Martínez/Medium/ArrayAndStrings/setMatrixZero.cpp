db3bae01ddb89d46e704d9a7974d13eb5cdd30af
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-13 20:10:25 -0600

    Solucion agregada

--- David Martínez/Medium/ArrayAndStrings/setMatrixZero.cpp
+++ David Martínez/Medium/ArrayAndStrings/setMatrixZero.cpp
@@ -2,3 +2,133 @@
 #include <set>
 #include <vector>
 using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        //1 listen
+            //Recibo una matriz de tamaño (n * m)
+            //Si un elemento tiene un valor de 0
+                //Convierto su columna y su renglon en 0s
+            //Debe ser in-place
+        
+            //m y n, pueden tener rango entre 1 y 200
+            //m = numero de renglones
+            //n = numero de columnas
+        
+        //2 example
+            //Cuaderno
+        
+        //3 brute force
+            //Puedo regorrer todos los elementos de la matriz y guardar en donde las voy encontrando
+            //Guardo los indeces de renglones y columnas donde todo lo convierto a 0
+            //Runtime: O(n*m)
+            //Memory: (O(n))
+            
+        //4 optimize
+            //Siento que la unica manera de optimzarlo es hacerlo todo en una corrida
+            //En el momento no se me ocurre una manera de hacerlo O(m+n)
+                //Sería recorrer todos los renglones y luego todas las columnas
+        
+            //Optimización de memoria siento que puede ser, recorrerlo todo lineal y
+                //Apenas encuentre un 0, convierto todo su renglon a 0 y su columna
+                //Mientras convierto su renglon a 0 o su columna a 0. Checo si hay otro 0
+                
+            
+        
+        //5 walkthrough
+            //Inicializo un vector que guarda las columnas
+            //Inicializo un vector que guarda los renglones
+            //Recorro y guardo las posiciones donde existen digitos 0
+            //Luego borro esos digitos selectivamente
+        
+        //6 implement
+        
+        //7 test
+            //Special Cases
+                //Renglones con size 0
+                //Comlumnas con size 0
+        
+        //Vectores renglones y columnas
+            //vector<int> renglones = {};
+            //vector<int> columnas = {};
+        
+        //Es más eficiente que no se repitan, usaré set pero dejo comentada opción vectores
+        //Da igual si uso set o unorderes_set. La busqueda sera O(1), no n(log n). porque los index
+            //Index son numeros ordenados
+        
+        //runtime: O(n*m)
+        //Memory: O(n)
+        
+        //Set de renglones y columnas
+        set<int> renglones = {};
+        set<int> columnas = {};
+        
+        //Saco el numero de 
+        int m = matrix.size();  //Renglones
+        int n = matrix[0].size();   //Columnas
+        
+        //Checo si ambos son de size 1
+        if(n == 1 && m == 1){ return; }
+        
+        //Comienzo la lectura de la matriz. runtime: O(n*m)
+        //Recorro renglones
+        for(int i = 0; i < m; i++){
+            //Recorro columnas
+            for(int j = 0; j < n; j++){
+                //Si encuentro un 0
+                if(matrix[i][j] == 0){
+                    renglones.emplace(i); //Agrego el renglon a renglones
+                    columnas.emplace(j);  //Agrego la columna a columnas
+                }    
+            }
+        }
+        
+        
+        //Ahora comienzo el proceso de eliminación. runtime: O(n*m)
+        
+        //Checo size de renglon y de columnas
+        //Si renglones == m o columnas == n
+        //Ahora todo lo hago 0
+        if(m == renglones.size() || n == columnas.size()){
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        //Si no
+        else{        
+            //columnas
+            //Recorro todos los renglones
+            for(int i = 0; i < m; i++){
+                //Recorro las columnas a borrar
+                for(auto it = columnas.begin(); it != columnas.end(); it++){
+                    matrix[i][*it] = 0;
+                }
+                /*
+                for(int j = 0; j < columnas.size(); j++){
+                    matrix[i][columnas[j]] = 0;   //Igualo a cero esos valores
+                }
+                */
+            }
+        
+            //Renglones
+        
+            //Recorro los renglones a borrar
+            for(auto it = renglones.begin(); it != renglones.end(); it++){
+                vector<int> aux(n, 0);
+                
+                matrix[*it] = aux;
+            }
+            /*
+            for(int i = 0; i < renglones.size(); i++){
+                int renglon = renglones[i];
+                vector<int> aux(n) = {0};
+                
+                matrix[renglon] = aux;
+            }  
+            */
+        }        
+    }
+};

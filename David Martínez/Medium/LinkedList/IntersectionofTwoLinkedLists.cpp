1c3ec9652e728066b9b98bc9ccfb088c3e84641a
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-26 15:15:39 -0600

    Agregada solución no optima

--- David Martínez/Medium/LinkedList/IntersectionofTwoLinkedLists.cpp
+++ David Martínez/Medium/LinkedList/IntersectionofTwoLinkedLists.cpp
@@ -9,3 +9,155 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        /*
+        // 1. listen
+            input: head of two singly linked lists
+            output: node value where both linked lists intersect, 
+                    if there isn`t a intersection return null
+        
+            There are no cycled linked lists
+            
+            Linked Lists must retain their original structure
+            
+            Both linked lists can have differente sizes (between 1 and 30,000)
+            
+            Nodo se encuentra entre el inicio y final, de cada lista
+            
+        
+        // 2. example
+        
+            input:
+            [2,3,4,5,6,8,2]
+            [4,5,7,8,9,1]
+            
+            output: 4
+            
+        // 3. brute force
+            
+            //Using hashtables
+            Hashtable for one linked list
+                
+                value = Pointer to thar node
+            
+                Traverse one list and add each value to the hashtable
+                Traverse the next list
+                    if the value exists in the hashtable, return the value
+            
+                return nullptr;
+            
+                //runtime: O(n+m)
+                //memory: O(n)
+            
+            //Traverse one list for each element
+            
+            For each node in one list, traverse the other list
+                //runtime: O(n*m)
+                //memory: O(1)
+            
+            
+        // 4. optimize
+            //BCR
+                Best conceivable runtime: O(n+m)
+            
+            //Possibly a slow pointer and a fast pointer
+                Debo pensar en las propiedades de linked lists, no me salío por no pensar eso
+                
+        // 5. walkthrough
+        // 6. implement
+        // 7. test 
+            Valor al inicio de una lista
+            Valor no se encuentra en las listas 
+        */
+        
+        /*
+        //Hashtable
+            //runtime: O(n+m)
+            //memory: O(n)
+            
+        unordered_map<ListNode*, int> hashTable;
+        
+        ListNode * curr = headA;
+        
+        while( curr != nullptr){
+
+            if(hashTable.find(curr) == hashTable.end()){
+                hashTable[curr] = curr->val;
+            }
+            
+            curr = curr->next;
+        }
+        
+        curr = headB;
+        
+        while(curr != nullptr){
+            
+            if(hashTable.find(curr) != hashTable.end()){
+                
+                return curr;
+
+            }
+            curr = curr->next;
+
+        }
+        
+        return nullptr;
+        */
+        
+        /*
+        //Math logic
+            //Runtime: O(n+m)
+            //Memory: O(1)
+            
+        //Size of each linked list
+        int sizeA = 1;
+        int sizeB = 1;
+        
+        ListNode * curr = headA;
+        
+        while(curr != nullptr){
+            sizeA++;
+            curr = curr->next;
+        }
+        
+        curr = headB;
+        
+        while(curr != nullptr){
+            sizeB++;
+            curr = curr->next;
+        }
+        
+        //Diference in sizes     
+        int diff;
+        ListNode * aux;
+        
+        if(sizeA > sizeB){
+            diff = sizeA - sizeB;
+            curr = headA;
+            aux = headB;
+        }else{
+            diff = sizeB - sizeA;
+            curr = headB;
+            aux = headA;
+        }
+        
+        for(int i = 0; i < diff; i++){
+            
+            curr = curr->next;
+        }
+        
+        while(curr != nullptr){
+            if(curr == aux){
+                return curr;
+            }
+            curr = curr->next;
+            aux = aux->next;
+        }
+        
+        return nullptr;
+        */
+    }
+};

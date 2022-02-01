1341863143d57e42bac7c03c600cba0d5c6b6b8c
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-02-01 12:22:40 -0600

    Agregada solución optima

--- David Martínez/Easy/Trees/SymmetricTree.cpp
+++ David Martínez/Easy/Trees/SymmetricTree.cpp
@@ -10,3 +10,27 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        
+        return isSymmetricHelper(root->left, root->right);
+    }
+    
+    bool isSymmetricHelper(TreeNode * left, TreeNode * right){
+        
+        //Si alguno de ambos lados es nullptr
+        if(left == nullptr || right == nullptr){
+            return left == right;   //Regreso si son iguales
+        }
+        
+        //Si los valores no son iguales
+        if(left->val != right->val){
+            return false;   //Regreso false
+        }
+        
+        //Llamo la función de nuevo y paso los valores a comparar
+        return (isSymmetricHelper(left->left, right->right)) && (isSymmetricHelper(left->right, right->left));
+        
+    }
+};

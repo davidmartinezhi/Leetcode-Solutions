ddc5a0d5622d5de68529772c83f3d7a565acbf21
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-02-15 14:43:29 -0600

    initial setup

---
+++ David Martínez/Medium/TreesAndGraphs/BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(): val(0), left(nullptr), right(nullptr) {};
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {};
+};

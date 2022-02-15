7668a4efd9837c228deb5e6b9af65def90c08c8e
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-02-15 14:43:57 -0600

    Agregada Solución

--- David Martínez/Medium/TreesAndGraphs/BinaryTreeZigzagLevelOrderTraversal.cpp
+++ David Martínez/Medium/TreesAndGraphs/BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -11,3 +11,128 @@ struct TreeNode {
     TreeNode(): val(0), left(nullptr), right(nullptr) {};
     TreeNode(int x): val(x), left(nullptr), right(nullptr) {};
 };
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        /*
+        1. listen
+            input: root of a binary tree
+            output: zig zag level order traversal of node values in a vector of vectors
+            
+            left -> right
+            right -> left
+            
+            Number of nodes in the tree varies from 0 to 200
+            Node val of each node varies from -100 to 100
+            
+        2. example
+            
+             3
+            / \
+           9  20
+              / \
+             15  17
+             
+        [[3], [20, 9], [15, 17]]
+        
+            nullptr
+            []
+            
+            1
+            [[1]]
+        
+        3. brute force
+            level by level traversal using queue
+            use a counter to know if the nodes should be added from left->right or right->left
+            runtime: O(n)
+            memory: O(n)
+                Only the resulting vector as default, as extra memory
+                And the largest possible queue size
+                
+        
+        4.optimize
+            I feel that the brute force proposed solution in efficient enough
+        
+        5.Walkthrough
+        
+        queue that will have the level nodes
+        result vector
+        counter starting from 0
+        
+        while queue is not empty
+            get current size (nodes in level)
+            add 1 to the counter
+            auxiliary vector
+            
+
+            
+            for loop of nodes on that level
+                add left and right child if they exist, to the queue
+                    odd: left->right
+                    even: right->left
+                
+                add node value to the aux vector
+            
+            append aux vector to result
+        
+        6.implement
+        
+        7.test         
+            Special cases:
+                -empty root
+            
+            To-do:
+                Check if Q.front() is correct syntax
+        */
+        
+        if(root == nullptr){
+            return {};
+        }
+
+        
+        //Vector for result
+        vector<vector<int>> result;
+        
+        //Counter
+        int counter = 0;
+        
+        //Queue for level by level traversal
+        queue<TreeNode *> Q; 
+        Q.push(root);
+        
+        while(!Q.empty()){
+            
+            int nodes = Q.size();    //Nodes on the current level
+            counter++;  //Increment counter
+            vector<int> auxVector = {};
+            
+            for(int i = 0; i < nodes; i++){
+                TreeNode *aux = Q.front();
+                Q.pop();
+                auxVector.push_back(aux->val);
+            
+                if(aux->left) {Q.push(aux->left);}
+                if(aux->right) {Q.push(aux->right);}                
+            }
+            
+            if(counter % 2 == 0){
+                int start = 0;
+                int end = auxVector.size() - 1;
+                while(start < end){
+                    int auxIndex = auxVector[start];
+                    auxVector[start] = auxVector[end];
+                    auxVector[end] = auxIndex;
+                    start++;
+                    end--;
+                }
+            }
+            
+            result.push_back(auxVector);   
+                
+        }
+        
+        return result;
+        
+    }
+};

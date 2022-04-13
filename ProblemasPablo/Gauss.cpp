ef7f541753dd2566f457ad735b4dd7ccc9732e20
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-12 19:29:23 -0500

    Agregada formula con enteros y con punto decimal

--- ProblemasPablo/Gauss.cpp
+++ ProblemasPablo/Gauss.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 
 double gauss(double n){
-    double aux = (n+1)/2;
+    double aux = (n+1)/2;   //De esta manera lo puedo sacar con numeros con punto decimal
     return  aux * n;
+}
+
+int gaussInt(int n){
+    return n * (n+1) / 2;   //De esta manero lo puedo sacar con enteros
 }
 
 int main()
 {
     cout << endl;
-    cout << " Gauss: " << gauss(8) << endl;
+    cout << " Gauss: " << gaussInt(6) << endl;
     cout << endl;
     return 0;
 }

1a733625fd2d66814eaff00280b55398280f56bf
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-13 13:47:44 -0500

    Agregada solución optima

--- David Martínez/Medium/ArrayAndStrings/ContainerWithMostWater.cpp
+++ David Martínez/Medium/ArrayAndStrings/ContainerWithMostWater.cpp
@@ -2,3 +2,121 @@
 #include <vector>
 using namespace std;
 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        /*
+        1. info
+            input: integer array "height" of length "n"
+            
+            each index represents a line
+            
+            find 2 lines that together with the x axis form a container
+                such that the container contains the most water
+                
+            output: maximum amount of water a container can store
+            
+            n == heigh.length
+            n <= n <= 100,000
+            0 <= height[i] <= 10,000
+        
+        2. example
+            notebook
+            
+        3. brute force
+            nested for loops to check for all possible water containers
+            runntime: O(n^2)
+            space: O(1)
+            
+        4. optimize
+        
+            Two pointer technique (pointer at beginning and at the end)
+            the pointer at the index with the shortest line * window is the current window
+            compare current window to max window
+            
+            time: O(n)
+            space: O(1)
+            
+            bcr: O(n)
+            
+        5. walkthrough
+            
+            move pointer with that points to the shortest line
+            if both lines are the same
+                compare next values
+                    if the biggest next, is the pointer that moves or if the line is equal as previous
+        6. implement
+        
+        7. test
+            -all values are the same
+            -pointers have the same value
+            -pointers have different values
+        */
+        
+        
+        //pointers
+        int start = 0;
+        int end = height.size() - 1;
+        
+        //max cointainer variable
+        int maxContainer = INT_MIN;
+        
+        //window variable
+        int window = height.size() - 1;
+        
+        //traversal moving start when both indexes have the same number
+        while(start < end){
+            
+            //Calculate current container and compare with maxContainer
+            int minLine = min(height[start], height[end]);
+            int currContainer = window * minLine;
+            
+            if(currContainer > maxContainer) maxContainer = currContainer;
+            
+            //Move shortest pointer
+            if(height[start] < height[end]){
+                start++;
+            }
+            else if(height[start > height[end]]){
+                end--;
+            }
+            else{   //If both indexes have the same value
+                start++;
+            }
+            
+            window--;
+        }
+
+        //pointers
+        start = 0;
+        end = height.size() - 1;
+
+        //window variable
+        window = height.size() - 1;
+        
+        //traversal moving end when both indexes have the same number
+        while(start < end){
+            
+            //Calculate current container and compare with maxContainer
+            int minLine = min(height[start], height[end]);
+            int currContainer = window * minLine;
+            
+            if(currContainer > maxContainer) maxContainer = currContainer;
+            
+            //Move shortest pointer
+            if(height[start] < height[end]){
+                start++;
+            }
+            else if(height[start > height[end]]){
+                end--;
+            }
+            else{   //If both indexes have the same value
+                end--;
+            }
+            
+            window--;
+        }
+        
+        return maxContainer;
+    }
+};

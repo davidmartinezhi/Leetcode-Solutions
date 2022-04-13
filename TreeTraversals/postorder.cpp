24e1b64558423e06d2345dcb1a3b4c6096622dfd
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-13 14:25:26 -0500

    Agregado traversal

--- TreeTraversals/postorder.cpp
+++ TreeTraversals/postorder.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 struct TreeNode {
@@ -11,3 +12,62 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+        //Recursive
+        /*
+        vector<int> result;
+        postorder(root, result);
+        return result;
+        */
+        
+        if(root == nullptr) return {};
+
+        //Iterative
+        vector<int> result;
+        stack<TreeNode*> stack;
+        stack.push(root);
+        
+        //traversal
+        while(!stack.empty()){
+            
+            while(root != nullptr){
+                
+                result.push_back(root->val);
+                stack.push(root);
+                root = root->right;
+                
+            }
+            
+            root = stack.top();
+            stack.pop();
+            root = root->left;
+        }
+        
+        int i = 0, j = result.size()-1;
+        while(i < j){
+            int aux = result[i];
+            result[i] = result[j];
+            result[j] = aux;
+            i++;
+            j--;
+        }
+        
+        
+        return result;
+    }
+    
+    void postorder(TreeNode * root, vector<int> & result){
+        
+        //Base case
+        if(root == nullptr) return;
+        
+        //Recursive step
+        postorder(root->left, result);
+        postorder(root->right, result);
+        result.push_back(root->val);
+        
+    }
+};

362902fc611064c60ef583a36ab59fa21f80e336
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-15 19:01:47 -0500

    agregada solucion2

--- David Martínez/Medium/DynamicProgramming/HouseRobberIII.cpp
+++ David Martínez/Medium/DynamicProgramming/HouseRobberIII.cpp
@@ -16,7 +16,7 @@ struct TreeNode{
     TreeNode(int x, TreeNode* leftP, TreeNode *rightP): val(x), left(leftP), right(rightP) {};
 };
 
-class Solution{
+class SolutionArray{
     int rob(TreeNode* root) {
     /*
     1. listen
@@ -90,3 +90,82 @@ y mme comento que no hay duda de mis problem solving abilities. Trabajaré más 
 en la planeación del inicio.
     -Solo escribir lo escencial
 */
+
+
+class SolutionPair {
+public:
+    int rob(TreeNode* root) {
+    /*
+    1. listen
+    
+        input: only one entrance, which is the "root"
+            each house has only one parent house, forming a binary tree
+            
+        output: max amount of money that can be robbed without alerting the police
+        
+        contraints: nodes in the binary tree are between 1 and 10,000
+                    node values are between 0 and 10,000 also
+    
+    2. example
+        notebook
+        
+        [3,2,3,null,3,null,1]
+        -> 7
+        
+    3. brute force
+        dfs and compare returning values
+        O(n) time
+        O(n) extra space
+    
+    4.optimize
+        bcr: O(n)
+        
+    5. walkthrough
+        dfs left and right
+        
+        compare fathe + prevleft + prevright and actual left+ actual right
+        return the max
+        
+        if father + prevs is bigger, the actuals become prev
+        if actuals are bigger, actuals become the prev
+        
+    7. test
+        tree of one node
+        
+    */
+        pair<int,int> money;
+        money.first = 0;
+        money.second = 0;
+        return robHelper(root, money).second;
+        
+    }
+    
+    pair<int,int> robHelper(TreeNode* root, pair<int,int> money) {
+        
+        //base case
+        if(root == nullptr) return {0,0};
+        
+        pair<int,int> aux;
+        aux.first = 0;
+        aux.second = 0;
+        
+        //values variables
+        pair<int,int> left = robHelper(root->left, aux);
+        pair<int,int> right = robHelper(root->right, aux);
+        
+        //compare
+        int actual = root->val + left.first + right.first;
+        int prev = left.second + right.second;
+        
+        if(actual > prev){
+            aux.first = prev;
+            aux.second = actual;
+            return aux;
+        }
+        
+        aux.first = prev;
+        aux.second = prev;        
+        return aux;
+    }
+};
+

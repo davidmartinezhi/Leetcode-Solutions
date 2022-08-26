fbce4c81fd9cede37b89637879796e36e50e0d86
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-08-26 10:25:18 -0500

    agregada solución con kmp

--- David Martínez/Easy/String/ImplementStrStr.cpp
+++ David Martínez/Easy/String/ImplementStrStr.cpp
@@ -1,146 +1,149 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        //1 listen
-            //Return the index of the first ocurrance of needle in haystack
-            //If needle isn't in haystack, return -1
-            //If needle is an empty string, we return 0
-            //Haystack and needle sizes, ranges between 0 and 50,000
-        
-        //2 example
-            // haystack: "hello", needle: "ll" -> 2
-            // haystack: "abbccdcdcdcd", needle: "cd" -> 4
-            
-        //3 brute force
-            //Sliding window tecnique and when the substring matches, we return the index.
-            //Memory: O(1)
-            //Runtime: O(n)
-
-        //4 optimize
-            //BCR O(n)
-            //If the string was sorted, we could use binary search
-                
-        
-        //5 walkthrough
-            //Get size of needle, to create the window
-            //If size of needle is 0, return 0
-            //Compare it to the haystack size, if neddale is bigger, we return -1
-            //Traverse the haystack in search for the needle
-                //If found, we return the index
-            //If not found, return -1
-        
-        //6 implement
-            //Siento que con pointers, podría no tener que usar el for loope
-        
-        //7 test
-            //needle not in haystack
-            //needle in haystack
-            //Special cases:
-                //Haystack empty
-                //needle empty
-                //Haystack is smaller than needle
-        
-            //To do:
-                //Double check the condition in for loop
-        
-        
-        //Creado manualmente
         /*
-        //Eficiencia de runtime: O(n-window * window)
-        //Eficiencia memoria: O(1)
-
+        1. listen
         
-        int window = needle.size();
-        
-        //Check if needle is empty
-        if(window == 0){ return 0; }
-        
-        //Check if needle is bigger than the haystack
-        if(window > haystack.size()){ return -1; }
-        
-        //Traverse the string
-        for(int i = 0; i <= haystack.size() - window; i++){
-            //I coul just do a loop inside this one, 0 < window. and compara haystack[i] and needle[j]
+            implement strStr()
+            input: input haystack and input needle. both are strings
+            output: index of the first ocurrance of the needle in haystack. -1 if needle is not found
             
-            //To optimize I could just check for characters who start with the same letter as window
-            if(haystack[i] == needle[0]){
-                //If the substring is found
-                if(isSubstring(haystack, needle, i, i + window)){
-                    return i;   //Return index
-                }                
+            return 0, when needle is an empty string
+            
+            hay and needls size are in range 0 and 50,000
+            they consist of only lowercase english letters
+            
+        2. example
+        
+            "hello", needle = "ll"
+            -> 2
+            
+            "aaaaa", needle = "bba"
+            -> -1
+            
+            haystack = "", needle = ""
+            -> 0
+        
+        3. brute force
+            For each char in haystack, we traverse the needle
+            If a char is different, we break
+            
+            runtime: O(m*n)
+            memory: O(1)
+        
+        4. optimize
+            traverse haystack, if a char is equal to the first char of the needle
+            we traverse that part.
+            
+            
+            runtime: O(n+m)
+            memory: O(1)
+            
+        5. walkthrough
+            traverse haystack until a char is the same as the first char in needle
+            Create a pointer for the traversal of needle
+            if the pointer is equal to needle size-1. return i
+            
+        6. implement
+        
+        7. test
+            empty needle *
+            needle not in haystack *
+            needle in haystack*
+            needle is haytack*
+            empty haystack and non-empty needle *
+            needle is longer than haystack
+            
+            checklist:
+                j == needle.size() or j == needle.size() - 1
+        */
+        
+        //Este es el brute force
+        /*
+        //Check if needle is empty
+        if(needle.size() == 0) return 0;
+        
+        //Check if needle is greater than haystack
+        if(needle.size() > haystack.size()) return -1;
+        
+        //O(n)
+        for(int i = 0; i < haystack.size() - needle.size()+1; i++){
+            int j = 0;
+            
+            while(haystack[i+j] == needle[j] && i+j < haystack.size()){
+                j++;
+            }
+            
+            if(j == needle.size()) return i;
+        }
+        
+        return -1;  //needle not found
+        */
+        
+        if(needle.size() == 0) return 0;
+        
+        //Set up LPS table
+        int n = needle.size();
+        vector<int> LPS(n, 0); //Memory: O(m) 
+        int prevLPS = 0, i = 1;
+        
+        //Runtime: O(m)
+        while(i < n){
+            if(needle[i] == needle[prevLPS]){
+                LPS[i] = prevLPS + 1;
+                i++;
+                prevLPS++;
+            }
+            else{
+                if(prevLPS == 0){
+                    LPS[i] = 0;
+                    i++;
+                }
+                else{
+                    prevLPS = LPS[prevLPS-1];
+                }
             }
         }
         
-        return -1;  //If needle not found, return -1
-        */
+        //Traverse the haystack
+        //Ponter at the beginning of each word
+        i = 0; //haystack
+        int j = 0; //pointer for needle
         
-        //Solución con el nested for loop
-        
-        int window = needle.size();
-        
-        //Check if needle is empty
-        if(window == 0){ return 0; }
-        
-        //Check if needle is bigger than the haystack
-        if(window > haystack.size()){ return -1; }
-        
-        //Traverse the string
-        for(int i = 0; i <= haystack.size() - window; i++){
-            //Creo variable j
-            int j = 0;
-            
-            if(haystack[i] == needle[0]){   //Si estoy en la letra con la que inicia el needle
-                for(; j < window; j++){ //Recorro para checar los demas characters
-                    
-                    if(haystack[i+j] != needle[j]){ //Si es diferente el character, salgo
-                        break;
-                    }
+        //Runtime: O(n)
+        while(i < haystack.size()){
+            if(haystack[i] == needle[j]){
+                i++;
+                j++;
+            }
+            else{
+                if(j == 0){
+                    i++;
+                } else{
+                    j = LPS[j-1];
                 }
             }
             
-            if(j == window){ return i; }    //Si J, tiene el mismo tamaño de la window. 
-                                            //Significa que encontramos el substring
-            
-        }
-        return -1;  //Si recorremos todo el string y no encontramos nada, regreso 0
-    }
-    
-    //Helper funciton to traverse substring
-    bool isSubstring(string & haystack, string & needle, int & start, int  end){
-        int i = start;
-        int j = 0;
-        
-        while(i < end){
-            if(haystack[i] != needle[j]){
-                return false;
+            if(j == needle.size()){
+                return i-j;
             }
-            i++;
-            j++;
         }
-        return true;
+        
+        return -1;
+
     }
 };
-
 /*
 Notas:
-    -Terminado en 32 minutos.
-    -Me tarde haciendo la helper function. La ahice para que se viera más estetico el codigo
-    -Hubiera primero creado el nested for loop.
-        y que solo entrará al loop cuando estamos en una letra igual a la primera letra del haystack
-    
-    -Se me hizo bastante sencillo el problema
-    -Noté que despues de batallar durante 2 días con String to Integer (ATOI), afecto mi confidence
-        No debería permitir que me afecte, ya aprendí a trabajar mejor y a ser más cuidadoso. 
-        Obvio no todo problema saldra rapido o con la maxima eficiencia, pero debo de esforzarme por que sea así.
-        Pero voy puliendo mi approach a la solución de problemas y estoy aprendiendo de cada uno, no hay porque desanimarse
+28 min brute force
 
+Resulta que existe un algoritmo para solucionar esto
+KMP string matching algorithm, lo reduce a n el runtime
 
-    -Es una solución bastante eficiente, ya que el mejor runtime posible es O(n) y lo tengo en O(n-window * window). Consultar este runtime.
-    - Memory usado = O(1)
-
-    Checar una posible solución más eficiente, para ver si la puedo acercar a O(n).
-
+Es muy bueno aprender algoritmos utiles como el de Kadane y este
 */

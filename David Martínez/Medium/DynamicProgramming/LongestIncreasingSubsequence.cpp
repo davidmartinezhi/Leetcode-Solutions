f309e175c894217d2596582bfb7f6157ca506264
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-09-17 18:52:22 -0500

    solucion optima

--- David Martínez/Medium/DynamicProgramming/LongestIncreasingSubsequence.cpp
+++ David Martínez/Medium/DynamicProgramming/LongestIncreasingSubsequence.cpp
@@ -4,6 +4,32 @@
 using namespace std;
 
 
-
-
-
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+        //declare aux vector for dp
+        vector<int> aux(nums.size(), 1);
+        
+        //declare varibale for longest subsequence
+        int longest = 1;
+        
+        //traverse the array
+        for(int i = 1; i < nums.size(); i++){
+            
+            //traverse from first index to i
+            for(int j = 0; j < i; j++){
+                
+                //if the front index is bigger, and it has a shortest or equal subsequence count
+                if(nums[i] > nums[j] && aux[i] <= aux[j]){
+                    aux[i] = aux[j] + 1;    //add the subsequence of the smaller number and add 1
+                    
+                    longest = max(longest, aux[i]); //save the number of longest subsequence
+                }
+            }
+        }
+        
+        //return longest subsequence
+        return longest;
+    }
+};

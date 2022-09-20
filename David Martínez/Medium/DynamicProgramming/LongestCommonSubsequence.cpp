4c7eec7881b04de14e48eb7d0bd4995ecbc0c1dd
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-09-20 18:05:40 -0500

    agregada solución optima

--- David Martínez/Medium/DynamicProgramming/LongestCommonSubsequence.cpp
+++ David Martínez/Medium/DynamicProgramming/LongestCommonSubsequence.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        //declare variables
+        
+        //lengths
+        int len1 = text1.size();
+        int len2 = text2.size();
+        
+        //dp table
+        int dp[len1+1][len2+1];
+        
+        //traverse and fill table
+        for(int i = 0; i <= len1; i++){
+            for(int j = 0; j <= len2; j++){
+                
+                //first row and columns
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                //match
+                else if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                //no match
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[len1][len2];
+    }
+};

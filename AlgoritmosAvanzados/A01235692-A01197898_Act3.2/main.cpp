9a80be03f18a978e8d90b941873c9630a25fc524
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-10-06 12:47:22 -0500

    added comment

--- AlgoritmosAvanzados/A01235692-A01197898_Act3.2/main.cpp
+++ AlgoritmosAvanzados/A01235692-A01197898_Act3.2/main.cpp
@@ -1,6 +1,7 @@
 /*
 Act3.2
 Implement Dijkstra and Floyd-Warshall algorithms
+(Added Dijkstra solution using adjacency list just for the fun of it, it's commented on line 132)
 
 The program must read a number n followed by n x n nonnegative integer values that represent an adjacency matrix of a directed graph.
 The first number represents the number of nodes, the following values in the array, the value at position i,j represents the weight of the arc from node i to node j. 
@@ -9,7 +10,7 @@ If there is no arc between node i and node j, the value in the array must be -1
 David Gerardo Martínez Hidrogo - A01235692 
 Humberto Ricardo Rodriguez Ruán - A01197898
 
-Created: Oct 5, 2022 at 12:54pm    Last Moidification: Oct 5, 2022
+Created: Oct 5, 2022 at 12:54pm    Last Modification: Oct 6, 2022 12:46pm
 
 */
 

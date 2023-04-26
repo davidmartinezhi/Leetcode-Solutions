fcb1d8a58ecead20a7c8bd65efcd51d136e28031
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-04-25 23:12:05 -0600

    agregada solución de subsets

---
+++ David Martínez/Medium/Backtracking/subsets.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        /*
+        info
+            input: integer array "nums" of unique elements
+            output: all possible subsets
+            constraints:
+                *solution must not contain duplicate subsets
+                *solution can be in any order
+                *array length is between 1 and 10
+                *all the numbers in the array are unique
+                
+        example
+            nums = [1,2,3]
+            -> [[],[1], [2], [3], [1,2], [1,3], [1,2,3]]
+            
+        brute force
+            
+            recursive solution
+            backtracking fits because its a state space tree
+            
+            complexity:
+                runtime: O(n^n)
+                extra space: O(recursive calls)
+                
+        optmimize
+            backtracking is the only way we'll find all permutations
+            use memoization to optimize memory
+            
+        walkthrough
+            traverse array
+                add, send to recursive call
+                don't add, send to recursive call
+            
+        test
+            * array length one
+            * array length more than one
+        
+        */
+        
+        vector<vector<int>> result;
+        vector<int> subset;
+        
+        backtracking(result, nums, subset, 0);
+        
+        return result;
+        
+        
+    }
+    
+    void backtracking(vector<vector<int>> & result, vector<int> & nums,vector<int> subset, int idx){
+        
+        if(idx >= nums.size()){
+            result.push_back(subset);
+            return;
+        }
+        
+        subset.push_back(nums[idx]);
+        backtracking(result, nums, subset, idx+1);
+        subset.pop_back();
+            
+        backtracking(result, nums, subset, idx+1);
+        
+        
+    }
+};

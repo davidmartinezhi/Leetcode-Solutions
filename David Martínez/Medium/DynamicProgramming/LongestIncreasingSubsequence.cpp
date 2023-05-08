ad6c90509972f36649d9945af4f316ec1dbdcb37
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-08 11:48:47 -0600

    added notes and new code

--- David Martínez/Medium/DynamicProgramming/LongestIncreasingSubsequence.cpp
+++ David Martínez/Medium/DynamicProgramming/LongestIncreasingSubsequence.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 class Solution {
 public:
+
+//2022
+    /*
     int lengthOfLIS(vector<int>& nums) {
         
         //declare aux vector for dp
@@ -34,6 +37,80 @@ public:
         //return longest subsequence
         return longest;
     }
+    */
+
+//2023
+    int lengthOfLIS(vector<int>& nums) {
+        /*
+        info
+            input: integer array "nums"
+            output: length of longest strictly increasing subsequence
+            constraints: 
+                * array length in range 1 and 2500
+                * nums[i] values in range -10,000 and 10,000
+                
+        example
+           nums = [10,9,2,5,3,7,101,18] 
+           -> 4
+           
+           nums = [0,1,0,3,2,3]
+           -> 4
+           
+           nums = [7,7,7,7,7,7,7]
+           -> 1
+           
+        brute force
+            dynammic programming
+            
+            nums = [0,1,0,3,2,3]
+            
+            dp = [1,2,2,3,3,4]
+            
+            complexity:
+                runtime: O(n^2)
+                extra space: O(n)
+                
+        walkthrough
+        
+            for each number
+                traverse previous numbers
+                    if(value in array is lesser than current value)
+                        value in dp array is max of currValue idx and value at position of value + 1
+                        if value bigger than max value, last index is maxValue
+                        save max value at last index
+                        
+        
+        test
+            *Carry max value
+            *Don't carry max calue
+        
+        */
+        
+        
+        /*
+            nums = [0,1,0,3,2,3] 
+            dp =   [0,1,0,2,2,3]
+            maxValue = 2
+        
+        */
+        
+        
+        //declare variables for dp
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int maxValue = 1;
+        
+        //traverse original array
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){ //traverse previous values before current idx
+                if(nums[j] < nums[i]){ 
+                    dp[i] = max(dp[j]+1, dp[i]);
+                    maxValue = max(dp[i], maxValue);
+                }   
+            }
+        }
+        return maxValue;
+    }
 };
 
 /*
@@ -54,4 +131,10 @@ Notas:
     En increasing subsequence, solo es, hasta el index actual. por eso esa es la diferencia.
 
     Debería de repasar dp y hacer una lista pequeña de patrones importantes y el setup
+
+
+
+8 meses despues volcí a ver este problema y lo resolví en 20 minutos.
+El concepto lo entendói bien y la solución quedo aún más limpia.
+A seguir trabajando.
 */

c2783382a4f1b025fa9c55c937f56966d9177f89
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-08-22 18:22:43 -0500

    added optimal DFS Solution

--- David Martínez/Medium/Graphs/PacificAtlanticWaterFlows.cpp
+++ David Martínez/Medium/Graphs/PacificAtlanticWaterFlows.cpp
@@ -54,8 +54,24 @@ public:
         //declare variables
         int rows = heights.size();
         int cols = heights[0].size();
+
+        vector<vector<bool>> pacific(rows, vector<bool>(cols, false));
+        vector<vector<bool>> atlantic(rows, vector<bool>(cols, false));
         vector<vector<int>> flowToBothOceans;
 
+        //marcar bordes izquierdos y derechos
+        for(int row = 0; row < rows; row++){
+            dfs(heights, pacific, row, 0, INT_MIN);
+            dfs(heights, atlantic, row, cols-1, INT_MIN);
+        }
+
+        //marcar bordes superior e inferior
+        for(int col = 0; col < cols; col++){
+            dfs(heights, pacific, 0, col, INT_MIN);
+            dfs(heights, atlantic, rows-1, col, INT_MIN); 
+        }
+
+        /*
         //traverse matrix
         for(int row = 0; row < rows; row++){
             for(int col = 0; col < cols; col++){
@@ -67,9 +83,45 @@ public:
                 }
             }
         }
+        */
+
+
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                if(pacific[row][col] && atlantic[row][col]){
+                    flowToBothOceans.push_back({row, col});
+                }
+            }
+        }
 
         return flowToBothOceans;
     }
+
+
+    // Función DFS para explorar si una celda puede llegar a los océanos Pacífico y/o Atlántico
+    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& visited, int i, int j, int prevHeight) {
+        // Caso base: Verificar si estamos fuera de los límites de la matriz
+        if (i < 0 || i >= heights.size() || j < 0 || j >= heights[0].size())
+            return;
+        
+        // Caso base: Verificar si la celda ya ha sido visitada o si su altura es menor que la altura previa
+        if (visited[i][j] || heights[i][j] < prevHeight)
+            return;
+
+        // Marcar la celda como visitada
+        visited[i][j] = true;
+        
+        // Si ya hemos llegado a ambos océanos, no hay necesidad de seguir explorando
+        //if (isPacific && isAtlantic)
+          //  return;
+
+        // Llamar a DFS en las celdas adyacentes (arriba, abajo, izquierda, derecha)
+        dfs(heights, visited, i - 1, j, heights[i][j]); // Arriba
+        dfs(heights, visited, i + 1, j, heights[i][j]); // Abajo
+        dfs(heights, visited, i, j - 1, heights[i][j]); // Izquierda
+        dfs(heights, visited, i, j + 1, heights[i][j]); // Derecha
+    }
+
 
     void reachesBothOceans(vector<vector<int>> & heights, int & rows, int & cols, bool &pacific, bool &atlantic, int row, int col, int prev){
 
@@ -88,9 +140,12 @@ public:
         //base case: check if we have reached both oceans
         if(pacific && atlantic) return;
 
+        //base case: already visited
+        if(heights[row][col] == INT_MAX) return;
+
         int curr = heights[row][col];
 
-        //base case: check if curr its greater or equal to last value
+        //check if curr its greater or equal to last value
         if(curr > prev) return;
 
         //change value in matrix

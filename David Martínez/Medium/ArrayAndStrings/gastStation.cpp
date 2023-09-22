7e65f5b6bcbd01f3f630527bc9e1812890a0014b
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-09-22 14:20:52 -0600

    notes, complexity and review of solution

--- David Martínez/Medium/ArrayAndStrings/gastStation.cpp
+++ David Martínez/Medium/ArrayAndStrings/gastStation.cpp
@@ -87,3 +87,33 @@ public:
         return (totalGas < totalCost) ? -1 : start;
     }
 };
+
+/*
+time: 45+
+
+complexity:
+    runtime: O(n)
+    extra space: O(1)
+
+        
+            gas = [1,2,3,4,5]
+            cost =[3,4,5,1,2]
+            totalGas = 1+2=3+3=6+4=10+5=15
+            totalCost = 3+4=7+5=12+1=13+2=15
+            tank = 4-1=3+5=8-2=6
+            start = 3
+
+            i = 4
+
+            if there is more total gas than cost, it's possible
+            use kadanes algorithm to mantain starting idx of trip
+
+
+I had really no idea how could i get the solution with complexity of O(n)
+and extra space of O(1), i thought it must be a dp solution to achieve O(n)
+
+I will reflect more on this problem to find how could I identify faster the
+possible solution
+
+
+*/

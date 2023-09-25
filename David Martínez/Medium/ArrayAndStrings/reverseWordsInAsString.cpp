97b55179f0ed36105a76f5dcd77f888ce9dd4d94
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-09-25 12:05:33 -0600

    added optimal solution

--- David Martínez/Medium/ArrayAndStrings/reverseWordsInAsString.cpp
+++ David Martínez/Medium/ArrayAndStrings/reverseWordsInAsString.cpp
@@ -1,3 +1,94 @@
 #include <iostream>
-using namespaces std;
+using namespace std;
 
+class Solution {
+public:
+    string reverseWords(string s) {
+        /*
+        info
+            input:
+                * strng "s"
+
+            output:
+                * reverse order of words
+                * should only have single space separating the words
+
+            constraints:
+                * what range of sizes can s have? [1, 10k]
+                * what type of chars can s have? english letters, digits and spaces
+                * there is at least one word
+
+        example
+            s = "the sky is blue"
+            -> blue is sky the
+
+            s = "  hello world  "
+            -> world hello
+
+        brute force
+            traverse the array to get all words and store them in an array
+            then concatenate the words in a result is reverse order
+                traversing the array backwards
+
+            complexity:
+                runtime: O(n)
+                extra space: O(n)
+
+        optimize:
+            best conceivable runtime: O(n)
+            can i improve memory? I think yes but while traversing backwards
+
+        test
+            * reverse two words 
+            * reverse multiple words
+        */
+
+        //get size
+        int n = s.size();
+
+        //declare variables
+        string wordsReversed = "";
+        int wordEnd = INT_MAX;
+        int fisrtWordIdx = 0;
+        /*
+            s = "  hello world  "
+                        i
+                             e
+        */
+
+        //find first word idx
+        for(int i = 0; i < n; i++){
+            if(s[i] != ' '){
+                fisrtWordIdx = i;
+                break;
+            } 
+        }
+
+        //traverse backwards
+        for(int i = n-1; i >= 0; i--){
+
+            //if we beggin a word, set wordEnd
+            if(s[i] != ' ' && wordEnd == INT_MAX){
+                wordEnd = i;
+            }
+
+            //if we end a word update word end and get previous word
+            if(s[i] == ' ' && wordEnd != INT_MAX){
+                wordsReversed += s.substr(i+1, wordEnd-i);
+                wordEnd = INT_MAX; //update word end
+
+                if(i > fisrtWordIdx){
+                    wordsReversed += " ";
+                }
+            }
+        }
+
+        if(s[0] != ' '){
+            wordsReversed += s.substr(0, wordEnd+1);
+        }
+
+        return wordsReversed;
+    }
+};
+
+//16 minutes

3b848a80751d97bbb97d2931ba0ed79429b5abdb
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-10-27 18:55:05 -0600

    included libraries

---
+++ David Martínez/Medium/LinkedList/removeDuplicatesFromSortedList.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
